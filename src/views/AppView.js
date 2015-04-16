b487140586e77f100e365f5710c74b780e372cc6
RussHR <russ.rinzler@gmail.com>
2015-04-15 22:53:05 -0700

    page slides in and out with click on hamburger

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -11,12 +11,37 @@ define(function(require, exports, module) {
   function AppView() {
     View.apply(this, arguments);
 
+    this.menuToggle = false;
+
     _createPageView.call(this);
     _setListeners.call(this);
   }
 
   AppView.prototype = Object.create(View.prototype);
   AppView.prototype.constructor = AppView;
+
+  AppView.prototype.toggleMenu = function() {
+    if(this.menuToggle) {
+      this.slideLeft();
+    } else {
+      this.slideRight();
+    }
+    this.menuToggle = !this.menuToggle;
+  };
+
+  AppView.prototype.slideRight = function() {
+    this.pageModifier.setTransform(Transform.translate(276, 0, 0), {
+      duration: 300,
+      curve: 'easeOut'
+    });
+  };
+
+  AppView.prototype.slideLeft = function() {
+    this.pageModifier.setTransform(Transform.translate(0, 0, 0), {
+      duration: 300,
+      curve: 'easeOut'
+    });
+  };
 
   AppView.DEFAULT_OPTIONS = {};
 
@@ -28,9 +53,7 @@ define(function(require, exports, module) {
   }
 
   function _setListeners() {
-    this.pageView.on('menuToggle', function(){
-      console.log('menu toggle!');
-    }.bind(this));
+    this.pageView.on('menuToggle', this.toggleMenu.bind(this));
   }
 
   module.exports = AppView;
