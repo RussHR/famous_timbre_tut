be546ce7395f1e782b6f9e7de15a5c8fa473bfd2
RussHR <russ.rinzler@gmail.com>
2015-04-15 22:07:38 -0700

    we gots a header bar

--- src/views/PageView.js
+++ src/views/PageView.js
@@ -1,19 +1,51 @@
 /*** PageView.js ***/
 
 define(function(require, exports, module) {
-    var View = require('famous/core/View');
-    var Surface = require('famous/core/Surface');
-    var Transform = require('famous/core/Transform');
-    var StateModifier = require('famous/modifiers/StateModifier');
+  var View            = require('famous/core/View');
+  var Surface         = require('famous/core/Surface');
+  var Transform       = require('famous/core/Transform');
+  var StateModifier   = require('famous/modifiers/StateModifier');
+  var HeaderFooter    = require('famous/views/HeaderFooterLayout');
 
-    function PageView() {
-        View.apply(this, arguments);
-    }
+  function PageView() {
+    View.apply(this, arguments);
 
-    PageView.prototype = Object.create(View.prototype);
-    PageView.prototype.constructor = PageView;
+    _createLayout.call(this);
+    _createHeader.call(this);
+  }
 
-    PageView.DEFAULT_OPTIONS = {};
+  PageView.prototype = Object.create(View.prototype);
+  PageView.prototype.constructor = PageView;
 
-    module.exports = PageView;
+  PageView.DEFAULT_OPTIONS = {
+    headerSize: 44
+  };
+
+  function _createLayout() {
+    this.layout = new HeaderFooter({
+      headerSize: this.options.headerSize
+    });
+
+    var layoutModifier = new StateModifier({
+      transform: Transform.translate(0, 0, 0.1)
+    });
+
+    this.add(layoutModifier).add(this.layout);
+  }
+
+  function _createHeader() {
+    var backgroundSurface = new Surface({
+      properties: {
+        backgroundColor: 'black'
+      }
+    });
+
+    var backgroundModifier = new StateModifier({
+      transform: Transform.behind
+    });
+
+    this.layout.header.add(backgroundModifier).add(backgroundSurface);
+   }
+
+  module.exports = PageView;
 });
